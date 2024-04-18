c1b5691cc0a92b6be17b6b81c57518ea23c8bccd
Hao-Ting Tung <haotingtong30@gmail.com>
2024-04-18 10:35:48 +0800

    Add: JWT authorization (#1)

--- src/meet_team_api/models/user.py
+++ src/meet_team_api/models/user.py
@@ -2,15 +2,51 @@
 
 from pydantic import BaseModel
 
+from .course import ListedCourse
 
-class RegisterRequest(BaseModel):
-    """This is the model for register function"""
+# Typedef
+UserId = int
+
+
+class UserRequestBaseModel(BaseModel):
+    """This is the model for user-related request"""
+
+    account: str
+    password: str
+
+
+class UserBaseModel(BaseModel):
+    """This is the model for User model"""
+
+    id: UserId
     name: str
-    account: str
-    password: str
 
 
-class LoginRequest(BaseModel):
+class RegisterRequest(UserRequestBaseModel):
+    """This is the model for register function"""
+
+    name: str
+
+
+class LoginRequest(UserRequestBaseModel):
     """This is the model for login function"""
+
+
+class ListedUserItem(UserBaseModel):
+    """some docstring"""
+
+
+class UserInfoGuest(BaseModel):
+    """This model is for getting user info in user page"""
+
+    description: str
+    avg_rating: float
+
+
+class UserInfoSelf(BaseModel):
+    """This model is for getting user info in user page"""
+
     account: str
-    password: str
+    description: str
+    courses: list[ListedCourse]
+    avg_rating: float
